a8ba72411f4ae5aeb0cb12857e3ca245f335d33d
LGL <gogs@fake.local>
2017-05-11 00:02:42 +0800

    添加新的内容

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,13 +11,16 @@ Page({
     console.log("---------")
     var that=this;
   },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onReady: function () {
+    wx.navigateTo({
+      url: '../login/login?id=1'
+    })
+  },
   onLoad: function () {
     console.log('onLoad')
-    // wx.navigateTo({
-    //   url: '../login/login?id=1'
-    // })
-
-    
     // var that = this
     // //调用应用实例的方法获取全局数据
     // app.getUserInfo(function(userInfo){
--- pages/investment/investment.js
+++ pages/investment/investment.js
@@ -5,17 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    progress:25,
+    btn_visibel: false,
+    asks: [
+      [{ checked: false, text: "选项一"}, { checked: false, text: "选项2"}],
+      [{ checked: false, text: "选项3"}, { checked: false, text: "选项4"}],
+      [{ checked: false, text: "选项5"}, { checked: false, text: "选项6"}],
+      [{ checked: false, text: "选项3"}, { checked: false, text: "选项4"}]
     ],
-    indicatorDots: false,
-    autoplay: false,
-    interval: 5000,
-    duration: 1000
+    current :0,
+    ask_text:"问题1"
+   
   },
+  EventHandle: function (event){
+    console.log((100 / 4) * (event.detail.current+1));
+    this.setData({
+      progress: (100 / 4) * (event.detail.current + 1),
+      ask_text: "问题" + (event.detail.current + 1)
+    });
 
+    
+    this.setData({
+      btn_visibel: (event.detail.current + 1)==4?true:false
+    });
+    // console.log(event.detail.current);
+  },
+  aaa:function(){
+    
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
